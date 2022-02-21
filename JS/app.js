1b53fcb27012775ad3396edeab29fb95badcaf20
Omar Elkony <omarelkony@hotmail.com>
2022-02-22 00:59:13 +0200

    sum shit

--- JS/app.js
+++ JS/app.js
@@ -1,37 +1,54 @@
 const country = document.getElementById('country')
 const countryContainer = document.querySelector('.countries')
+const body = document.body;
 let countryList = []
+let cs = []
 
-function Hi(){
-    const xml = new XMLHttpRequest();
-    xml.open('GET',`https://restcountries.com/v3.1/all`)
-    xml.onload = Getter;
-    xml.send();
 
-    function Getter(){
-        const data = JSON.parse(this.responseText)
-        console.log(data)
+const xml = new XMLHttpRequest();
+xml.open('GET',`https://restcountries.com/v3.1/all`)
+xml.onload = Getter;
+xml.send();
+
+function Getter(){
+    const data = JSON.parse(this.responseText)
+    console.log(data)
         let text = ''
         data.forEach(c => countryList.push(c))
         countryList.sort((a,b)=>a.name.common>b.name.common?1:-1)
-        countryList.forEach(cou => {
-            text += 
-            `
-            <div class="country">
-                <img src="${cou.flags.png}">
-                <div class="c-details">
-                    <h2>${cou.name.common}</h2>
-                    <p><strong>Capital:</strong> ${cou.capital}</p>
-                    <p><strong>Region:</strong> ${cou.region}</p>
-                    <p><strong>Population:</strong> ${cou.population}</p>
-                </div>
+    countryList.forEach(cou => {
+        text += 
+        `
+        <div class="country" id="${cou.name.common}">
+            <img src="${cou.flags.png}">
+            <div class="c-details">
+                <h2>${cou.name.common}</h2>
+                <p><strong>Capital:</strong> ${cou.capital}</p>
+                <p><strong>Region:</strong> ${cou.region}</p>
+                <p><strong>Population:</strong> ${cou.population}</p>
             </div>
-
-            `
-        })
-        countryContainer.innerHTML = text;
-        
-    }      
+        </div>
+        `
+    })
+    countryContainer.innerHTML = text;
 }
 
-Hi()
+
+
+// This Function gets the scroll depth of the page and if its greater than
+// the distance between the top and the search bar it makes its position fixed
+function searchFixed(){
+    let y = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    let searchBar = document.querySelector('.search')
+    if(y>68){
+        searchBar.style.position = 'fixed'
+        searchBar.style.top = '0'
+    }else{
+        searchBar.style.position = 'relative'
+    }
+}
+//The event listener of the previous Function
+window.addEventListener('scroll',searchFixed)
+
+
+
